e3c18411d42f05cf0d625b8a17b894728a78a6a7
kyunglok78 <7878lok@gmail.com>
2026-07-08 09:36:23 +0900

    second
    second

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-// 전역 시스템 키 (파이썬 서버를 쓰면 HTML에는 키가 노출될 필요 없으나 예비용으로 둠)
+// 전역 시스템 키
 const GLOBAL_KAKAO_KEY = "c48e895cae6e16bc82fc1ed089539352";
 const GLOBAL_DATA_KEY = "1oOOTG+s4eO71/a5Xn4xV2l7/d+k0G8w3QZ2+29e612B1R0nOq91xH5t5WwI/n5yD9vP4o7zQ7s6+V6e+368lA==";
 
--- js/location.js
+++ js/location.js
@@ -4,12 +4,14 @@ function syncContractor(val) {
     });
 }
 
+// [기능 5-5] 동적 주입 로우 내 더미 문자열 공백 초기화 처리
 function createLocationRowHTML(index) {
     return `
         <div class="list-row">
-            <input type="checkbox" class="row-checkbox"><span>소재지 ${index}</span><input type="text" class="input-short" value="신규 사업장 ${index}">
+            <input type="checkbox" class="row-checkbox"><span>소재지 ${index}</span>
+            <input type="text" class="input-short" placeholder="예: 공장명/지점명">
             <button type="button" class="btn-blue" onclick="openAddressModal(this); return false;"><i class="fa-solid fa-magnifying-glass"></i> 주소 검색</button>
-            <span>주소</span><input type="text" class="input-long addr-input" value="">
+            <span>주소</span><input type="text" class="input-long addr-input" placeholder="주소 검색 버튼을 이용해 리얼 주소를 맵핑해 주세요" value="" readonly>
             <div class="check-group">
                 <label class="check-item"><input type="checkbox" class="check-ledger" checked onchange="updateMenuState()"> 건축물대장</label>
                 <label class="check-item"><input type="checkbox" class="check-kfpa" checked onchange="updateMenuState()"> 화협자료평가</label>
